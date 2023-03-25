fcac8e73b60655875a8643dbc2af3ba1aa5a4f5d
HuFY-dev <127644049+HuFY-dev@users.noreply.github.com>
2023-03-25 00:09:00 -0400

    Reformatted in Black

--- link_predict.py
+++ link_predict.py
@@ -6,6 +6,7 @@ from dgl.nn import GraphConv
 from ogb.linkproppred import DglLinkPropPredDataset
 from torch.utils.data import DataLoader
 
+
 # Define the GCN model
 class GCN(nn.Module):
     def __init__(self, in_feats, emb_feats):
@@ -20,6 +21,7 @@ class GCN(nn.Module):
         x = F.relu(x)
         return x
 
+
 # Define the link prediction model
 class LinkPredictor(nn.Module):
     def __init__(self, emb_feats, out_feats):
@@ -32,6 +34,7 @@ class LinkPredictor(nn.Module):
         x = self.lin(x)
         return torch.sigmoid(x)
 
+
 def train(model, predictor, graph, split_edge, optimizer, batch_size):
     # Set the model to train mode
     model.train()
@@ -42,7 +45,7 @@ def train(model, predictor, graph, split_edge, optimizer, batch_size):
 
     # Get the train edges
     train_edges = split_edge["train"]["edge"].to(device)
-    train_feats = graph.ndata['feat']
+    train_feats = graph.ndata["feat"]
 
     # Initialize the loss and number of examples
     total_loss = total_examples = 0
@@ -61,7 +64,9 @@ def train(model, predictor, graph, split_edge, optimizer, batch_size):
         pos_loss = -torch.log(pos_out + 1e-15).mean()
 
         # Just do some trivial random sampling.
-        edge = torch.randint(0, graph.num_nodes(), edge.size(), dtype=torch.long, device=emb_x.device)
+        edge = torch.randint(
+            0, graph.num_nodes(), edge.size(), dtype=torch.long, device=emb_x.device
+        )
         neg_out = predictor(emb_x[edge[0]], emb_x[edge[1]])
         neg_loss = -torch.log(1 - neg_out + 1e-15).mean()
         loss = pos_loss + neg_loss
@@ -78,6 +83,7 @@ def train(model, predictor, graph, split_edge, optimizer, batch_size):
 
     return total_loss / total_examples
 
+
 def valid(model, predictor, graph, split_edge):
     # Set the model to evaluation mode
     model.eval()
@@ -88,7 +94,7 @@ def valid(model, predictor, graph, split_edge):
 
     # Get the valid edges
     valid_edges = split_edge["valid"]["edge"].to(device)
-    valid_feats = graph.ndata['feat']
+    valid_feats = graph.ndata["feat"]
 
     # Compute node embeddings
     emb_x = model(graph, valid_feats)
@@ -99,56 +105,62 @@ def valid(model, predictor, graph, split_edge):
     pos_loss = -torch.log(pos_out + 1e-15).mean()
 
     # Just do some trivial random sampling.
-    edge = torch.randint(0, graph.num_nodes(), edge.size(), dtype=torch.long, device=emb_x.device)
+    edge = torch.randint(
+        0, graph.num_nodes(), edge.size(), dtype=torch.long, device=emb_x.device
+    )
     neg_out = predictor(emb_x[edge[0]], emb_x[edge[1]])
     neg_loss = -torch.log(1 - neg_out + 1e-15).mean()
     loss = pos_loss + neg_loss
 
     return loss.item()
 
+
 def main():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Using device:", device)
 
     # Load dataset
-    dataset = DglLinkPropPredDataset(name='ogbl-collab')
+    dataset = DglLinkPropPredDataset(name="ogbl-collab")
     split_edge = dataset.get_edge_split()
     graph = dataset[0]
 
     # Prepare features
-    in_feats = graph.ndata['feat'].shape[1]
-    train_feats = graph.ndata['feat']
+    in_feats = graph.ndata["feat"].shape[1]
+    train_feats = graph.ndata["feat"]
 
     # Create model and optimizer
     emb_feats = 128
     model = GCN(in_feats, emb_feats).to(device)
     predictor = LinkPredictor(emb_feats, 1).to(device)
-    optimizer = torch.optim.Adam(list(model.parameters()) + list(predictor.parameters()), lr=0.0005)
+    optimizer = torch.optim.Adam(
+        list(model.parameters()) + list(predictor.parameters()), lr=0.0005
+    )
 
     # Train model
     graph = graph.to(device)
     batch_size = 4096
     for epoch in range(100):
         loss = train(model, predictor, graph, split_edge, optimizer, batch_size)
-        print(f'Epoch {epoch}, train loss: {loss}')
+        print(f"Epoch {epoch}, train loss: {loss}")
 
         # Validate model
         loss = valid(model, predictor, graph, split_edge)
-        print(f'Epoch {epoch}, valid loss: {loss}')
+        print(f"Epoch {epoch}, valid loss: {loss}")
 
         # Save the model checkpoint if epoch is multiple of 10
         if epoch % 10 != 0:
             continue
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'predictor_state_dict': predictor.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': loss,
-        }, f"GCN_checkpoint_epoch{epoch}.pt")
-    
-    
+        torch.save(
+            {
+                "epoch": epoch,
+                "model_state_dict": model.state_dict(),
+                "predictor_state_dict": predictor.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+                "loss": loss,
+            },
+            f"GCN_checkpoint_epoch{epoch}.pt",
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
